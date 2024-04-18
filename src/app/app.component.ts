93644e75c80ca4b31200b28a70478c342f43ea95
01-Hyphen <rahulshinde.rk.8899@gmail.com>
2024-04-18 20:51:17 +0530

    First commit with typing APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { faker } from '@faker-js/faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TypingApp';
+
+  randomText=faker.lorem.sentence();
+  enteredText="";
+
+
+  onTypingText(event:any){
+    this.enteredText=event.target.value
+    console.log(this.enteredText)
+  }
+  
+  compare(letter:any, enteredLetterIndex:any){
+    if(!this.enteredText[enteredLetterIndex]){
+      return "pending";
+    }else if(this.enteredText[enteredLetterIndex]==letter){
+      return "correct";
+    }else{
+      return "incorrect";
+    }
+  }
 }
